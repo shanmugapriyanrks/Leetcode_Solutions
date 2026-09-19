93fc79e0a9b0a3e4c93e80248dc72e055d968b23
SHANMUGAPRIYAN RK <shanmugapriyanrks@gmail.com>
2026-09-19 10:11:59 +0530

    Time: 0 ms (100%), Space: 10.1 MB (8.54%) - LeetHub

--- 0704-binary-search/0704-binary-search.c
+++ 0704-binary-search/0704-binary-search.c
@@ -1,22 +1,19 @@
 int search(int* nums, int numsSize, int target) {
-    int left = 0 ;
+    int left = 0;
     int right = numsSize - 1;
 
-    while (left <= right){
-        int mid = left +(right - left) / 2;
+    while (left <= right) {
+        int mid = (left + right)  / 2;
 
-    if(target == nums[mid])
-        return mid;
+        if (target == nums[mid])
+            return mid;
 
+        else if (target < nums[mid]) {
+            right = mid - 1;
 
-    else if(target < nums[mid]){
-        right = mid -1;
-
+        } else {
+            left = mid + 1;
+        }
     }
-    else{
-        left = mid + 1;
-    }
-    
-}
-return -1;
+    return -1;
 }
